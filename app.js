8d4ceafb9b6bea6cfe96fd8fbdea87abe6d531ec
Joseph M <jmedina1985@hotmail.com>
2021-03-05 14:07:46 -0800

    search functionality - refactoring

--- app.js
+++ app.js
@@ -27,8 +27,7 @@ async function fetchApi(url) {
   return data;
 }
 
-async function curatedPhotos() {
-  const data = fetchApi('https://api.pexels.com/v1/curated?per_page=15&page=1');
+function generatePictures(data) {
   data.photos.forEach((photo) => {
     const galleryImg = document.createElement('div');
     galleryImg.classList.add('gallery-img');
@@ -39,18 +38,18 @@ async function curatedPhotos() {
   });
 }
 
+async function curatedPhotos() {
+  const data = await fetchApi(
+    'https://api.pexels.com/v1/curated?per_page=15&page=1'
+  );
+  generatePictures(data);
+}
+
 async function searchPhotos(query) {
   const data = await fetchApi(
     `https://api.pexels.com/v1/search?query=${query}+query&per_page=15&page=1`
   );
-  data.photos.forEach((photo) => {
-    const galleryImg = document.createElement('div');
-    galleryImg.classList.add('gallery-img');
-    galleryImg.innerHTML = `<img src=${photo.src.large}></img>
-    <p>${photo.photographer}</p>
-    `;
-    gallery.appendChild(galleryImg);
-  });
+  generatePictures(data);
 }
 
 curatedPhotos();
