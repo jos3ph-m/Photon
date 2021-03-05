4ee05a611a5a1b612dd3f49b88898a1d2125976a
Joseph M <jmedina1985@hotmail.com>
2021-03-05 13:02:12 -0800

    search functionality

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const auth = '563492ad6f917000010000011f1e95b48cf4435fa24b8bc742ad5502';
 const gallery = document.querySelector('.gallery');
 const searchInput = document.querySelector('.search-input');
-const submitButton = document.querySelector('.submit-btn');
+const form = document.querySelector('.search-form');
 let searchValue;
 
 //Event Listeners
 searchInput.addEventListener('input', updateInput);
+form.addEventListener('submit', () => {
+  searchPhotos(searchValue)
+})
 
 function updateInput(e) {
   searchValue = e.target.value
