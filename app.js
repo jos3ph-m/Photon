7d9384491de70c3a89ae32c38b05cb568aa40047
Joseph M <jmedina1985@hotmail.com>
2021-03-05 12:34:00 -0800

    search functionality

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const searchInput = document.querySelector('.search-input');
 const submitButton = document.querySelector('.submit-btn');
 let searchValue;
 
+
+
 async function curatedPhotos() {
   const dataFetch = await fetch(
     'https://api.pexels.com/v1/curated?per_page=15&page=1',
@@ -28,7 +30,7 @@ async function curatedPhotos() {
 
 async function searchPhotos(query) {
   const dataFetch = await fetch(
-    'https://api.pexels.com/v1/search?query=${query}&per_page=15&page=1'
+    `https://api.pexels.com/v1/search?query=${query}+query&per_page=15&page=1`
     {
       method: 'GET',
       headers: {
@@ -36,7 +38,16 @@ async function searchPhotos(query) {
         Authorization: auth,
       },
     }
-  
+  )
+  const data = await dataFetch.json();
+  data.photos.forEach((photo) => {
+    const galleryImg = document.createElement('div');
+    galleryImg.classList.add('gallery-img');
+    galleryImg.innerHTML = `<img src=${photo.src.large}></img>
+    <p>%{photo.photographer}</p>
+    `;
+    gallery.appendChild(galleryImg);
+  });
 }
 
 curatedPhotos();
