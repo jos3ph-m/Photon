b77dc268bf39537f68b8255ab61c1c409842cc64
Joseph M <jmedina1985@hotmail.com>
2021-03-04 22:52:30 -0800

    setting constants in app.js

---
+++ app.js
@@ -0,0 +1,3 @@
+const auth = '563492ad6f917000010000011f1e95b48cf4435fa24b8bc742ad5502';
+const gallery = document.querySelector('.gallery');
+const searchInput = document.querySelector('.search-input');
